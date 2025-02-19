9c448eb17396b17c35efb6cd49a84366e5a31757
babucarr <babucarr.badjie12@gmail.com>
2025-02-20 00:24:39 +0800

    progress

--- chapter_3/src/routes/authRoutes.js
+++ chapter_3/src/routes/authRoutes.js
@@ -44,7 +44,26 @@ router.post("/login", (req, res) => {
   const { username, password } = req.body;
 
   try {
-    
+    const getUser = db.prepare("SELECT * FROM users WHERE username = ?");
+    const user = getUser.get(username);
+
+    // check if the user is not associated with that username
+    if (!user) {
+      return res.status(404).send({ message: "User not found" });
+    }
+
+    const passwordIsValid = bcrypt.compareSync(password, user.password);
+
+    // Checked if the password doesn't match
+    if (!passwordIsValid) {
+      return res.status(401).send({ message: "Invalid password" });
+    }
+
+    // successful authentication
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+      expiresIn: "24",
+    });
+    res.json({ token });
   } catch (err) {
     console.log(err.message);
     res.sendStatus(503);
