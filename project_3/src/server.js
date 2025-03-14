269a7169175ef14e7bc2a68df84ed77bd8ec3a51
babucarr <babucarr.badjie12@gmail.com>
2025-03-14 22:25:30 +0800

    project 3 created

---
+++ project_3/src/server.js
@@ -0,0 +1,36 @@
+import express from "express";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+import authRoutes from "./routes/authRoutes.js";
+import todoRoutes from "./routes/todoRoutes.js";
+import authMiddleware from "./middleware/authMiddleware.js";
+
+const app = express();
+const PORT = process.env.PORT || 5003;
+
+// Get the file path from the URL of the current module
+const __filename = fileURLToPath(import.meta.url);
+
+// Get the directory name from the file path
+const __dirname = dirname(__filename);
+
+// Middleware
+app.use(express.json());
+// Serves the HTML file from the /public directory
+// Tells express to serve all files from rhe public directory folder as static assets
+app.use(express.static(path.join(__dirname, "../public")));
+
+// Serving up the HTML file from the /public directory (index.html)
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+// Routes
+app.use("/auth", authRoutes);
+app.use("/todos", authMiddleware, todoRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port: ${PORT}`);
+});
+
+// to run the project ---> npm run dev
